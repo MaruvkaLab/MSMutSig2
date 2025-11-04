70eb08559a34b58e23a84010c9bb76cc85730612
Hagay-Ladany <49926189+Hagay-Ladany@users.noreply.github.com>
2025-11-04 13:12:14 +0200

    Modify input file and add threshold parameter
    Updated input file path and output directory for analysis. Added threshold parameter to the analysis script execution.

--- run_analysis/run_analysis.R
+++ run_analysis/run_analysis.R
@@ -3,19 +3,17 @@
 # Example Script to Run MSMuSig2 Analysis
 
 # Define Input and Output Paths
-input_file <- "../data/study_data.csv"
-output_dir <- "../results"
+input_file <- "../data/Loci.table.ALL_Only.2025.csv"
+output_dir <- "../results/All_2025/"
 selected_models <- "exponential,gaussian,weibull,lognormal" # Running all models (default)
 
 # Create Output Directories if They Don't Exist
-if (!dir.exists(output_dir)) {
-  dir.create(output_dir, recursive = TRUE)
-}
+if (!dir.exists(output_dir)) {  dir.create(output_dir, recursive = TRUE) }
 
 # Run the Analysis Script with Specified Parameters
 system(paste("Rscript", 
              "../scripts/analysis.R", 
              "--input", input_file, 
              "--output", output_dir, 
-             "--models", selected_models))
-
+             "--models", selected_models,
+             "--threshold", 40))
